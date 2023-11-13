057a239572f397d2b8ad25615b9a16b6e62b64d9
“AAndrews-1982 <altonandrews@icloud.com>
2023-11-13 10:58:44 -0600

    Lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,14 @@
+<div id="carouselControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <!-- Your carousel items here -->
+    </div>
+    <!-- Move the images outside the carousel container -->
+    <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+        <img src="path_to_your_previous_icon_image" alt="Previous" />
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+        <img src="path_to_your_next_icon_image" alt="Next" />
+        <span class="sr-only">Next</span>
+    </a>
+</div>

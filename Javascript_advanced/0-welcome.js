7746f7ad12eacaa5e6518b9c1c5c242919102dbd
“AAndrews-1982 <altonandrews@icloud.com>
2023-11-13 11:24:04 -0600

    pushed the wrong file for js

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,14 +1,15 @@
-<div id="carouselControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <!-- Your carousel items here -->
-    </div>
-    <!-- Move the images outside the carousel container -->
-    <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
-        <img src="path_to_your_previous_icon_image" alt="Previous" />
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
-        <img src="path_to_your_next_icon_image" alt="Next" />
-        <span class="sr-only">Next</span>
-    </a>
-</div>
+function welcome(firstName, lastName) {
+    // Concatenate first name and last name with a space
+    var fullName = firstName + ' ' + lastName;
+
+    // Function to display full name in an alert
+    function displayFullName() {
+        alert('Welcome ' + fullName + '!');
+    }
+
+    // Call the displayFullName function
+    displayFullName();
+}
+
+// Example usage
+welcome('John', 'Doe');
